8438e22dbaeb1b08c3f390a6bcce1143ac2b7831
zhyrkom <mikhail.zhirko@gmail.com>
2025-10-30 13:05:37 +0100

    Not full wdr lab4

---
+++ uwr_3sem/wdr/prac4/fractal_generator_single/target/release/deps/fractal_generator_single-b5dbc2c9a6879881.d
@@ -0,0 +1,5 @@
+/home/zhyrkom/Documents/study_repo/uwr_3sem/wdr/prac4/fractal_generator_single/target/release/deps/fractal_generator_single-b5dbc2c9a6879881.d: src/main.rs
+
+/home/zhyrkom/Documents/study_repo/uwr_3sem/wdr/prac4/fractal_generator_single/target/release/deps/fractal_generator_single-b5dbc2c9a6879881: src/main.rs
+
+src/main.rs:
